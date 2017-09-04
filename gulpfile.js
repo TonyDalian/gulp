2c2f450a031563d7c65e961889fdeff03e131d20
litao8976 <736188281@qq.com>
2017-09-04 23:48:16 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp')
 const sass = require('gulp-ruby-sass')
 
 gulp.task('sass', function () {
-    return sass('./sass/main.scss')//编译文件
-    .on('error', sass.logError)//错误信息
-    .pipe(gulp.dest('./css'))////输出路径
+    return sass('./sass/main.scss')
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./css'))
 })
 
 gulp.task('watch',function(){
-   gulp.watch('./sass/*.scss',['sass']);// 监听的文件
+   gulp.watch('./sass/*.scss',['sass']);
 });
